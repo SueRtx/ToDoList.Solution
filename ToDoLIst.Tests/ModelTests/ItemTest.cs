67fe25c1e9be042f09dbe56bb6b29f829f4ad9e7
Sue Roberts <suerobertstx@yahoo.com>
2022-06-24 14:30:14 -0500

    Pass Tests 1-3

--- ToDoLIst.Tests/ModelTests/ItemTest.cs
+++ ToDoLIst.Tests/ModelTests/ItemTest.cs
@@ -7,7 +7,42 @@ namespace ToDoList.Tests
   public class ItemTests
   {
 
-    // Test methods will go here.
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test"); //We pass in "test" as an argument here.
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+public void GetDescription_ReturnsDescription_String()
+{
+  //Arrange
+  string description = "Walk the dog.";
+  Item newItem = new Item(description);
+
+  //Act
+  string result = newItem.Description;
+
+  //Assert
+  Assert.AreEqual(description, result);
+}
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
 
   }
 }
