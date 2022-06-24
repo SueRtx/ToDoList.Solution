2f7021cb5e04766c3f7fae68320390f1ae927864
Sue Roberts <suerobertstx@yahoo.com>
2022-06-24 15:07:57 -0500

    Pass all Tests.

--- ToDoLIst.Tests/ModelTests/ItemTest.cs
+++ ToDoLIst.Tests/ModelTests/ItemTest.cs
@@ -1,32 +1,39 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using ToDoList.Models;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item("test"); //We pass in "test" as an argument here.
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
 
     [TestMethod]
-public void GetDescription_ReturnsDescription_String()
-{
-  //Arrange
-  string description = "Walk the dog.";
-  Item newItem = new Item(description);
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
 
-  //Act
-  string result = newItem.Description;
+      //Act
+      Item newItem = new Item(description);
+      string result = newItem.Description;
 
-  //Assert
-  Assert.AreEqual(description, result);
-}
+      //Assert
+      Assert.AreEqual(description, result);
+    }
 
     [TestMethod]
     public void SetDescription_SetDescription_String()
@@ -44,5 +51,34 @@ public void GetDescription_ReturnsDescription_String()
       Assert.AreEqual(updatedDescription, result);
     }
 
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      // Arrange
+      List<Item> newList = new List<Item> { };
+
+      // Act
+      List<Item> result = Item.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
